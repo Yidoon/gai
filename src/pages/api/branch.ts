d2d4640f4bd55b536750247a6e405d1feda0dc6d
邓奕东 <yidong.deng@tenclass.com>
2023-05-21 17:39:45 +0800

    diplay local and remote branch list

--- src/pages/api/branch.ts
+++ src/pages/api/branch.ts
@@ -1,72 +1,93 @@
 import { exec } from "child_process";
 import path from "path";
+import { NextApiRequest } from "next";
+import { Branch, BranchType } from "@/types/git";
 
 const EXCLUDE_BRANCHS = ["master", "dev", "stage", "uat", "develop"];
 export const LIST_LOCAL_BRANCHS = "git branch";
+export const LIST_REMOTE_BRANCHS = "git branch -r";
 const SPLITE_CHARACTER = "_cgb_";
 let CURRENT_BRANCH = "";
 
-// 获取本地仓库的分支列表
+/**
+ * @param branch
+ * @param path
+ * @returns
+ */
 const getBranchLatesCommit = (branch: string, path?: string) => {
   const cmdStr = `git log ${branch} --oneline --date=relative --pretty=format:"%h%${SPLITE_CHARACTER}%ad${SPLITE_CHARACTER}%s${SPLITE_CHARACTER}%ct" | head -n 1`;
 
   return new Promise((resolve, reject) => {
     exec(cmdStr, { cwd: path }, (err, stdout, stderr) => {
       const arr = stdout.split("_cgb_");
-      console.log(arr, "arr");
-      const obj = {
-        hash: arr[0].trim(),
-        date: arr[1].split("\n")[0],
-        date_unix: Number(arr[3].split("\n")[0]),
-        branch: branch,
-        subject: arr[2].trim(),
-      };
-      if (!err) {
+      try {
+        const obj = {
+          hash: arr?.[0]?.trim(),
+          date: arr?.[1]?.split("\n")[0],
+          date_unix: Number(arr?.[3]?.split("\n")[0]),
+          branch: branch,
+          subject: arr?.[2]?.trim(),
+        };
         resolve(obj);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    });
+  });
+};
+
+const getOriginBranchs = (
+  type: BranchType,
+  path: string
+): Promise<string[]> => {
+  const cmdStr = type === "local" ? LIST_LOCAL_BRANCHS : LIST_REMOTE_BRANCHS;
+  return new Promise((resolve, reject) => {
+    exec(cmdStr, { cwd: path }, (err, stdout, stderr) => {
+      if (!err) {
+        const list = stdout.split("\n").map((name: string) => {
+          return name.trim();
+        });
+        resolve(list);
       } else {
+        console.log(err);
         reject(err);
       }
     });
   });
 };
-const generateBranchList = (path?: string) => {
-  return new Promise((resolve, reject) => {
-    exec(LIST_LOCAL_BRANCHS, { cwd: path }, async (err, stdout, stderr) => {
-      if (err) {
-        return reject(err);
-      }
-      let branchList = stdout.split("\n").map((name: string) => {
-        return name.trim();
-      });
-      branchList = branchList.filter((name) => {
-        if (name.indexOf("*") > -1) {
-          CURRENT_BRANCH = name.replace("*", "").trim();
-        }
-        return name && !EXCLUDE_BRANCHS.includes(name) && name.indexOf("*") < 0;
-      });
-      const tempArr: any[] = [];
-      let tempRes;
-      for (let i = 0, len = branchList.length; i < len; i++) {
-        if (branchList[i]) {
-          tempRes = await getBranchLatesCommit(branchList[i], path);
-          tempArr.push(tempRes);
-        }
-      }
-      tempArr.sort((a, b) => a.date_unix - b.date_unix);
-      resolve(tempArr);
-    });
+const getBranchData = async (type: BranchType, path: string) => {
+  let originBranchs = await getOriginBranchs(type, path);
+  console.log(originBranchs.length, "originBranchs");
+  originBranchs.filter((name) => {
+    if (name.indexOf("*") > -1) {
+      CURRENT_BRANCH = name.replace("*", "").trim();
+    }
+    return name && !EXCLUDE_BRANCHS.includes(name) && name.indexOf("*") < 0;
   });
+  const resultArr = [];
+  let tempRes;
+  for (let i = 0, len = originBranchs.length; i < len; i++) {
+    if (originBranchs[i]) {
+      tempRes = await getBranchLatesCommit(originBranchs[i], path);
+      resultArr.push(tempRes);
+    }
+  }
+  return resultArr;
 };
 
-export default async function name(req: Request, res: any) {
+export default async function branch(req: NextApiRequest, res: any) {
   if (req.method === "GET") {
-    const projectPath = process.cwd();
+    const { projectPath, type } = req.query as {
+      projectPath: string;
+      type: BranchType;
+    };
     const mpDGclassPath = "/Users/Yidoon/Desktop/tenclass/mp-dgclass";
-    console.log(projectPath, "projectPath");
-    const branchList = await generateBranchList(mpDGclassPath);
-    console.log(branchList, "branchList");
+
+    const data = await getBranchData(type || "remote", mpDGclassPath);
+
     res.json({
-      data: branchList,
+      data: data,
       msg: "",
       code: 0,
     });
