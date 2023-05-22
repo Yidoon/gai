75085ab806f68e0d63de79bfe919bb73d3163c92
Yidoon <yidoongm@gmail.com>
2023-05-22 18:47:57 +0800

    Add filter

---
+++ src/pages/api/remote.ts
@@ -0,0 +1,48 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { resolve } from "path";
+import { exec } from "child_process";
+
+interface Remote {
+  [key: string]: {
+    type?: string;
+    url?: string;
+    [key: string]: string | undefined;
+  };
+}
+const getRemote = (path: string) => {
+  return new Promise((resolve, reject) => {
+    exec("git remote -v", (err, stdout, stderr) => {
+      if (err) {
+        reject(err);
+      } else if (stderr) {
+        reject(new Error(stderr));
+      } else {
+        const remote: Remote = {};
+        const remotes = stdout.trim().split("\n");
+        remotes.forEach((r) => {
+          const [name, url, type] = r.trim().split(/\s+/);
+          if (!remote[name]) {
+            remote[name] = {};
+          }
+          remote[name][type] = url;
+        });
+        resolve(remote);
+      }
+    });
+  });
+};
+
+export default async function source(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "GET") {
+    const { path } = req.query as { path: string };
+    const data = await getRemote(process.env.MOCK_PROJECT_PATH!);
+    res.json({
+      data: data,
+      msg: "",
+      code: 0,
+    });
+  }
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,13 +1,21 @@
+import AuthorSelect from "@/components/AuthorSelect";
+import BranchSourceSelect from "@/components/BranchSourceSelect";
 import { Branch } from "@/types/git";
-import { Button, Popconfirm, Table } from "antd";
+import { copyToClipboard } from "@/utils";
+import { CopyOutlined } from "@ant-design/icons";
+import { Button, Form, Popconfirm, Space, Table, Tag, message } from "antd";
 import axios from "axios";
+import dayjs from "dayjs";
 import { config } from "dotenv";
+import _ from "lodash";
 import React, { useEffect, useState } from "react";
 
 export default function Index() {
   const [branchs, setBranchs] = useState<Branch[]>([]);
   const [loading, setLoading] = useState(false);
   const [selectedRowKeys, setSelectedRowKeys] = useState<React.Key[]>([]);
+
+  const [form] = Form.useForm();
 
   const reqBranchs = async () => {
     setLoading(true);
@@ -17,7 +25,7 @@ export default function Index() {
     setLoading(false);
     setBranchs(res.data.data);
   };
-  const handleBranchDelete = async () => {
+  const handleBranchDelete = async (branchs: React.Key[]) => {
     const res = await axios.delete(
       `${process.env.NEXT_PUBLIC_API_HOST}/api/branch`,
       {
@@ -26,8 +34,14 @@ export default function Index() {
         },
       }
     );
-    console.log("affectedRows", res.data.data);
     reqBranchs();
+  };
+  const handleSelectSuggestionBranch = async () => {
+    const suggestBranchs = _.filter(branchs, (branch) => {
+      return branch.time < dayjs().subtract(5, "month").unix();
+    });
+    const selectedBranchs = _.map(suggestBranchs, "branch");
+    setSelectedRowKeys(selectedBranchs);
   };
 
   const columns = [
@@ -35,6 +49,15 @@ export default function Index() {
       title: "Branch",
       dataIndex: "branch",
       key: "branch",
+      render: (val: string) => {
+        const originName = val.split("/")[0];
+        return (
+          <div>
+            <Tag color="#2db7f5">{originName}</Tag>
+            <span>{val}</span>
+          </div>
+        );
+      },
     },
     {
       title: "Date",
@@ -50,21 +73,66 @@ export default function Index() {
       title: "Hash",
       dataIndex: "hash",
       key: "hash",
+      render: (val: string) => {
+        return (
+          <Space>
+            <span>{val.slice(0, 7)}</span>
+            {/* @ts-ignore */}
+            <CopyOutlined
+              className="cursor-pointer"
+              onClick={() => {
+                copyToClipboard(val);
+                message.success("复制成功");
+              }}
+            />
+          </Space>
+        );
+      },
+    },
+    {
+      title: "Time",
+      dataIndex: "time",
+      key: "time",
+      render: (value: number) => {
+        return dayjs.unix(value).format("YYYY-MM-DD HH:mm:ss");
+      },
     },
     {
       title: "Action",
       dataIndex: "branch",
       key: "action",
-      render: () => {
-        return <Button type="link"> 删除 </Button>;
+      render: (val: string) => {
+        return (
+          <Popconfirm
+            title="Delete the branch"
+            description="Are you sure to delete this branchs?"
+            okText="Yes"
+            cancelText="No"
+            onConfirm={() => {
+              handleBranchDelete([val]);
+            }}
+          >
+            <Button type="link"> 删除 </Button>
+          </Popconfirm>
+        );
       },
     },
   ];
   const rowSelection = {
     onChange: (selectedRowKeys: React.Key[], selectedRows: Branch[]) => {
-      console.log(selectedRowKeys, "selectedRowKeys");
       setSelectedRowKeys(selectedRowKeys);
     },
+    selectedRowKeys: selectedRowKeys,
+  };
+  const renderTableFooter = () => {
+    return selectedRowKeys.length > 0 ? (
+      <div>
+        已选 <span style={{ color: "#166cff" }}>{selectedRowKeys.length}</span>{" "}
+        个分支
+      </div>
+    ) : (
+      "暂未选择分支"
+    );
   };
   useEffect(() => {
     reqBranchs();
@@ -72,22 +140,42 @@ export default function Index() {
 
   return (
     <div className="p-4">
-      <div className="mb-4">
-        <Popconfirm
-          title="Delete the branchs"
-          description="Are you sure to delete this branchs?"
-          okText="Yes"
-          cancelText="No"
-          onConfirm={handleBranchDelete}
-        >
-          <Button disabled={selectedRowKeys.length === 0}>批量删除</Button>
-        </Popconfirm>
+      <div className="flex mb-4 justify-between">
+        <Form form={form} layout="inline">
+          <Form.Item name="author" label="Author">
+            <AuthorSelect />
+          </Form.Item>
+          <Form.Item name="source" label="Source">
+            <BranchSourceSelect />
+          </Form.Item>
+        </Form>
+        <div>
+          <Space size={12}>
+            <Popconfirm
+              title="Delete the branchs"
+              description="Are you sure to delete this branchs?"
+              okText="Yes"
+              cancelText="No"
+              onConfirm={() => {
+                handleBranchDelete(selectedRowKeys);
+              }}
+            >
+              <Button disabled={selectedRowKeys.length === 0}>
+                批量删除选中项
+              </Button>
+            </Popconfirm>
+            <Button type="primary" onClick={handleSelectSuggestionBranch}>
+              一键选中久远分支
+            </Button>
+          </Space>
+        </div>
       </div>
       <Table
         dataSource={branchs}
         columns={columns}
         rowKey="branch"
         loading={loading}
+        footer={renderTableFooter}
         rowSelection={{
           type: "checkbox",
           ...rowSelection,
