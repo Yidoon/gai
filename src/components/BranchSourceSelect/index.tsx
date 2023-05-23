610a8ec4f59862584720a5a6ce37b9c24bd3c784
Yidoon <yidoongm@gmail.com>
2023-05-23 22:24:05 +0800

    Add filter

--- src/components/BranchSourceSelect/index.tsx
+++ src/components/BranchSourceSelect/index.tsx
@@ -1,35 +1,52 @@
-import { Select } from "antd";
+import { BranchSourceListItem } from "@/types/git";
+import { Select, SelectProps } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
 const { Option } = Select;
 
-interface Props {}
+interface Props extends SelectProps {
+  dataSource?: BranchSourceListItem[];
+  disableRequest?: boolean;
+}
 const BranchSourceSelect = (props: Props) => {
-  const [branchSource, setBranchSource] = useState<string[]>([]);
-  const reqAuthors = async () => {
+  const { dataSource, disableRequest, ...restProps } = props;
+  const [branchSource, setBranchSource] = useState<BranchSourceListItem[]>([]);
+  const reqBranchSource = async () => {
     const res = await axios.get(
       `${process.env.NEXT_PUBLIC_API_HOST}/api/remote`
     );
     const sources = Object.keys(res.data.data).map((key) => {
-      return key;
+      return {
+        isRemote: true,
+        name: key,
+      };
     });
-    setBranchSource(sources.concat("local", "all"));
+    setBranchSource(
+      sources.concat([
+        { isRemote: false, name: "all" },
+        { isRemote: false, name: "local" },
+      ])
+    );
   };
 
   useEffect(() => {
-    reqAuthors();
-  }, []);
+    if (dataSource && disableRequest) {
+      setBranchSource(dataSource);
+    } else {
+      reqBranchSource();
+    }
+  }, [disableRequest, dataSource]);
 
-  const Options = branchSource.map((source) => {
+  const Options = branchSource.map((source, index) => {
     return (
-      <Option key={source} value={source}>
-        {source}
+      <Option key={index} value={source.name}>
+        {source.isRemote ? `remote/${source.name}` : source.name}
       </Option>
     );
   });
   return (
-    <Select {...props} style={{ width: 120 }}>
+    <Select {...restProps} style={{ width: 240 }}>
       {Options}
     </Select>
   );
